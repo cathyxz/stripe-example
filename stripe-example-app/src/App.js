c27122eda5084e8cfe1764bdf4c4f8aa8c050825
Cathy Zhu <cathyxz@gmail.com>
2017-07-31 21:20:27 -0700

    Use react router

--- stripe-example-app/src/App.js
+++ stripe-example-app/src/App.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
-import {StripeProvider} from 'react-stripe-elements';
+import { StripeProvider } from 'react-stripe-elements';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import MyCheckoutForm from './components/MyCheckoutForm.js'
-
+import Homepage from './components/Homepage.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <StripeProvider apiKey="pk_test_zoFVIfYbAVs0N5e91nkSvNEB">
-          <MyCheckoutForm/>
-        </StripeProvider>
+        <Switch>
+          <Route path="/" component={Homepage} />
+          <Route path="/checkout">
+            <StripeProvider apiKey="pk_test_zoFVIfYbAVs0N5e91nkSvNEB">
+              <MyCheckoutForm/>
+            </StripeProvider>
+          </Route>
+        </Switch>
       </div>
     );
   }
---
+++ stripe-example-app/src/components/Homepage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Homepage extends React.Component {
+
+  render() {
+    return (
+        <div>
+            <h1>Homepage</h1>
+            <div>
+                Welcome to Beijing
+            </div>
+        </div>
+    );
+  }
+};
+
+export default Homepage;
